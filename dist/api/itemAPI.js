1c070c12e937ae25937b32a3c737a6482117142d
Mikhail <mihail.kuznetsov.99@mail.ru>
2022-10-03 18:41:56 +0300

    new version

--- dist/api/itemAPI.js
+++
@@ -1,9 +0,0 @@
-export async function GetItem() {
-    try {
-        const response = await fetch('https://store.tildacdn.com/api/tgetproduct/');
-        const data = await response.json();
-        return data;
-    } catch(e) {
-        console.error(e);
-    }
-}
--- dist/index.js
+++ dist/index.js
@@ -1,29 +1,77 @@
-import { GetItem } from "./api/itemAPI.js";
+class UiProduct extends HTMLElement {
 
-const title = document.getElementById('title');
-const description = document.getElementById('description');
-const price = document.getElementById('price');
-const oldPrice = document.getElementById('oldPrice');
-const quantity = document.getElementById('quantity');
-const mainPhoto = document.getElementById('mainPhoto');
-const addBtn = document.getElementById('addBtn');
+    static get observedAttributes() { return ["images","product"]; }
 
-
-function SetValues(data) {
-    title.innerHTML = data.title
-    description.innerHTML = data.descr
-    if (data.quantity !== 0) {
-        quantity.innerHTML = `Only ${data.quantity} left`
-        price.innerHTML = `${data.price} USD`
-        oldPrice.innerHTML = `${data.priceold} USD`     
-    }else {
-        addBtn.style.display = "none"
+    get product() {
+      return JSON.parse(this.getAttribute("product"));
     }
-    const images = JSON.parse(data.images)
-    mainPhoto.src = images[0].img
+
+    set product(v) {
+        this.setAttribute("product", JSON.stringify(v));
+    }
+
+
+    changeMainImg(e) {
+        mainImg.src = e.src;
+        for (let i = 0; i < images.length; i++) {
+            images[i].classList.remove('active')
+        }
+        e.classList.add('active');
+    }
+
+    async fetchProduct() {
+        try {
+            const response = await fetch('https://store.tildacdn.com/api/tgetproduct/');
+            const data = await response.json();
+            this.product = data;
+            this.images = JSON.parse(data.images);
+        } catch(e) {
+            console.error(e);
+        }
+    }
+
+    async connectedCallback() {
+        await this.fetchProduct();
+        this.render();
+        const images = document.querySelectorAll('#images');
+        const mainImg = document.getElementById('mainImg');
+        images[0].classList.add('active')
+        images.forEach((e) => {
+            e.addEventListener('click', () => this.changeMainImg(event.target))
+        })
+    }
+
+    render() {
+        this.innerHTML = ` 
+        <section>
+        <div class="product">
+            <div class="product__image">
+                <div class="product__switch">
+                ${this.images.map((img) => {
+                    return `<img src="${img.img}" alt="headphones" id="images">`
+                })}
+                </div>
+                <div class="product__main-img">
+                    <img src="${this.images[0].img}" alt="Main Photo" id="mainImg">
+                </div>
+            </div>
+            <div class="product__main">
+                <h1 class="product__title" id="title">${this.product.title}</h1>
+                <div class="product__info">
+                    <div class="product__quantity" id="quantity">In stok: ${this.product.quantity}</div>
+                    <h3 class="product__description-title">Description:</h3>
+                    <span class="product__description" id="description">${this.product.descr}</span>
+                    <div class="product__price product-price">
+                        <span class="product-price__current" id="price">${this.product.price} USD</span>
+                        <span class="product-price__old" id="oldPrice">${this.product.priceold} USD</span>
+                    </div>
+                </div>
+                <button class="product__add-btn">Add to cart</button>
+            </div>
+        </div>
+    </section>
+        ` 
+    }
 }
-GetItem().then((data) => SetValues(data));
 
-
-
-
+customElements.define('ui-proguct', UiProduct)
